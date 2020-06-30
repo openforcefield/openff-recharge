a85c5bc60d88151cc2cc1fd47e4f90aaca592f2f
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-06-30 11:29:35 -0600

    Lint

--- openff/recharge/__init__.py
+++ openff/recharge/__init__.py
@@ -5,7 +5,8 @@ An automated framework for generating optimized partial charges for molecules.
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
--- openff/recharge/tests/test_openff_recharge.py
+++ openff/recharge/tests/test_openff_recharge.py
@@ -1,4 +1,5 @@
 def test_import_openff_recharge():
 
     from openff import recharge
+
     assert recharge is not None

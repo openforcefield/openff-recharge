3cdbf7c7c98fa25e64c7235f6f9be8a4ebc12b70
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-07-13 08:03:00 -0600

    Rename inverse_distance_matrix to sound like a function.

--- openff/recharge/utilities/geometry.py
+++ openff/recharge/utilities/geometry.py
@@ -1,7 +1,8 @@
 import numpy
 
 
-def inverse_distance_matrix(points_a: numpy.ndarray, points_b: numpy.ndarray):
+
+def compute_inverse_distance_matrix(points_a: numpy.ndarray, points_b: numpy.ndarray):
     """Computes a matrix of the inverse distances between all of the points
     in ``points_a`` and all of the points in ``points_b``.
 

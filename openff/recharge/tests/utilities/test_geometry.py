743f2b9d4f584cc92380ab4f3b6954a7e27f7be5
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-07-15 11:48:28 -0600

    Adds Utility to Re-order Geometry to Match a Tagged Molecule (#25)

--- openff/recharge/tests/utilities/test_geometry.py
+++ openff/recharge/tests/utilities/test_geometry.py
@@ -1,6 +1,10 @@
 import numpy
 
-from openff.recharge.utilities.geometry import compute_inverse_distance_matrix
+from openff.recharge.utilities.geometry import (
+    compute_inverse_distance_matrix,
+    reorder_conformer,
+)
+from openff.recharge.utilities.openeye import smiles_to_molecule
 
 
 def test_compute_inverse_distance_matrix():
@@ -16,3 +20,24 @@ def test_compute_inverse_distance_matrix():
     )
 
     assert numpy.allclose(expected, inverse_distances)
+
+
+def test_reorder_conformer():
+    """Tests that conformers can be correctly re-ordered."""
+
+    # Check the simple case where no re-ordering should occur.
+    oe_molecule = smiles_to_molecule("[C:1]([H:2])([H:3])([H:4])[H:5]")
+    conformer = numpy.array([[index, 0.0, 0.0] for index in range(5)])
+
+    reordered_conformer = reorder_conformer(oe_molecule, conformer)
+    assert numpy.allclose(reordered_conformer, conformer)
+
+    # Check a case where things are actually re-ordered simple case where no
+    # re-ordering should occur.
+    oe_molecule = smiles_to_molecule("[C:5]([H:1])([H:4])([H:2])[H:3]")
+
+    conformer = numpy.array([[index, 0.0, 0.0] for index in range(5)])
+    expected_conformer = conformer[numpy.array([4, 0, 3, 1, 2])]
+
+    reordered_conformer = reorder_conformer(oe_molecule, conformer)
+    assert numpy.allclose(reordered_conformer, expected_conformer)

69b7c70620396d5938a5b82a7a84b4547fd127d8
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-07-16 10:26:26 -0600

    Remove left-over Omega debug settings. Add precomputed conformer to charge test to speed up test.

--- openff/recharge/tests/charges/test_charges.py
+++ openff/recharge/tests/charges/test_charges.py
@@ -1,8 +1,8 @@
+import numpy
 import pytest
 from typing_extensions import Literal
 
 from openff.recharge.charges.charges import ChargeGenerator, ChargeSettings
-from openff.recharge.conformers.conformers import OmegaELF10
 from openff.recharge.utilities.openeye import smiles_to_molecule
 
 
@@ -12,6 +12,14 @@ def test_generate_charges(theory: Literal["am1", "am1bcc"]):
     the `ChargeGenerator` class."""
 
     oe_molecule = smiles_to_molecule("C")
-    conformers = OmegaELF10.generate(oe_molecule, 1)
+    conformer = numpy.array(
+        [
+            [-0.0000658, -0.0000061, 0.0000215],
+            [-0.0566733, 1.0873573, -0.0859463],
+            [0.6194599, -0.3971111, -0.8071615],
+            [-1.0042799, -0.4236047, -0.0695677],
+            [0.4415590, -0.2666354, 0.9626540],
+        ]
+    )
 
-    ChargeGenerator.generate(oe_molecule, conformers, ChargeSettings(theory=theory))
+    ChargeGenerator.generate(oe_molecule, [conformer], ChargeSettings(theory=theory))

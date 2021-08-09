6a3453173fc38d19b41792f61612f60f7d56a830
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-08-09 18:06:42 +0100

    Abstract tensor operations into new utility

--- openff/recharge/tests/utilities/test_geometry.py
+++ openff/recharge/tests/utilities/test_geometry.py
@@ -1,38 +1,60 @@
 import numpy
+import pytest
 
 from openff.recharge.utilities.geometry import (
-    compute_field_vector,
     compute_inverse_distance_matrix,
     compute_vector_field,
     reorder_conformer,
 )
 from openff.recharge.utilities.openeye import smiles_to_molecule
 
+try:
+    import torch
+except ImportError:
+    torch = None
 
-def test_compute_inverse_distance_matrix():
+tensor_types = [numpy.array] + ([] if torch is None else [torch.tensor])
 
-    points_a = numpy.array([[0.0, 1.0], [2.0, 3.0]])
-    points_b = numpy.array([[0.0, 2.0], [4.0, 6.0], [6.0, 8.0]])
 
-    inverse_distances = compute_inverse_distance_matrix(points_a, points_b)
-    assert inverse_distances.shape == (2, 3)
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_compute_inverse_distance_matrix(tensor_type):
 
-    expected = numpy.array(
+    points_a = tensor_type([[0.0, 1.0], [2.0, 3.0]])
+    points_b = tensor_type([[0.0, 2.0], [4.0, 6.0], [6.0, 8.0]])
+
+    expected_values = tensor_type(
         [[1.0000000, 0.1561738, 0.1084652], [0.4472136, 0.2773501, 0.1561738]]
     )
 
-    assert numpy.allclose(expected, inverse_distances)
+    inverse_distances = compute_inverse_distance_matrix(points_a, points_b)
+    assert inverse_distances.shape == expected_values.shape
+
+    assert numpy.allclose(expected_values, inverse_distances)
 
 
-def test_compute_field_vector():
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_compute_vector_field(tensor_type):
 
-    field_vector = compute_field_vector(
-        numpy.zeros((1, 3)), numpy.array([[3.0, 0.0, 0.0]])
+    points_a = tensor_type([[0.0, 3.0, 0.0], [0.0, 0.0, 4.0]])
+    points_b = tensor_type([[4.0, 0.0, 0.0]])
+
+    vector_field = compute_vector_field(points_a, points_b)
+    assert vector_field.shape == (1, 3, 2)
+
+    root_2 = numpy.sqrt(2)
+
+    expected_output = tensor_type(
+        [
+            [
+                [+4.0 / 5.0 ** 3, +4.0 / (root_2 * 4.0) ** 3],
+                [-3.0 / 5.0 ** 3, +0.0],
+                [+0.0, -4.0 / (root_2 * 4.0) ** 3],
+            ]
+        ]
     )
 
-    expected = numpy.array([[1.0 / 9.0, 0.0, 0.0]])
-
-    assert numpy.allclose(field_vector, expected)
+    assert vector_field.shape == expected_output.shape
+    assert numpy.allclose(expected_output, vector_field)
 
 
 def test_reorder_conformer():
@@ -54,15 +76,3 @@ def test_reorder_conformer():
 
     reordered_conformer = reorder_conformer(oe_molecule, conformer)
     assert numpy.allclose(reordered_conformer, expected_conformer)
-
-
-def test_compute_vector_field():
-
-    points_a = numpy.array([[1.0, 1.0, 0.0], [1.0, 0.0, 1.0]])
-    points_b = numpy.array([[1.0, 1.0, 1.0]])
-
-    vector_field = compute_vector_field(points_a, points_b)
-    assert vector_field.shape == (1, 3, 2)
-
-    expected = numpy.array([[0.0, 0.0, 1.0], [0.0, 1.0, 0.0]])
-    assert numpy.allclose(expected, vector_field[0, :, :].T)
---
+++ openff/recharge/tests/utilities/test_tensors.py
@@ -0,0 +1,120 @@
+import numpy
+import pytest
+
+from openff.recharge.utilities.tensors import (
+    append_zero,
+    cdist,
+    inverse_cdist,
+    pairwise_differences,
+    to_numpy,
+    to_torch,
+)
+
+try:
+    import torch
+except ImportError:
+    torch = None
+
+tensor_types = [numpy.array] + ([] if torch is None else [torch.tensor])
+
+
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_to_numpy(tensor_type):
+
+    input_tensor = tensor_type([[0.5, 1.0]])
+    output_tensor = to_numpy(input_tensor)
+
+    expected_tensor = numpy.array([[0.5, 1.0]])
+
+    assert output_tensor.shape == expected_tensor.shape
+    assert numpy.allclose(output_tensor, expected_tensor)
+
+
+def test_to_numpy_none():
+    assert to_numpy(None) is None
+
+
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_to_torch(tensor_type):
+
+    input_tensor = tensor_type([[0.5, 1.0]])
+    output_tensor = to_torch(input_tensor).type(torch.float64)
+
+    expected_tensor = torch.tensor([[0.5, 1.0]], dtype=torch.float64)
+
+    assert output_tensor.shape == expected_tensor.shape
+    assert torch.allclose(output_tensor, expected_tensor)
+
+
+def test_to_torch_none():
+    assert to_torch(None) is None
+
+
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_cdist(tensor_type):
+
+    input_tensor_a = tensor_type([[3.0, 0.0, 0.0], [4.0, 0.0, 0.0]])
+    input_tensor_b = tensor_type([[0.0, 2.0, 0.0], [0.0, 3.0, 0.0], [0.0, 4.0, 0.0]])
+
+    output_tensor = cdist(input_tensor_a, input_tensor_b)
+    assert type(output_tensor) == type(input_tensor_a)
+
+    output_tensor = to_numpy(output_tensor)
+
+    expected_tensor = numpy.array(
+        [[numpy.sqrt(x * x + y * y) for y in [2.0, 3.0, 4.0]] for x in [3.0, 4.0]]
+    )
+
+    assert output_tensor.shape == expected_tensor.shape
+    assert numpy.allclose(output_tensor, expected_tensor)
+
+
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_inverse_cdist(tensor_type):
+
+    input_tensor_a = tensor_type([[3.0, 0.0, 0.0], [4.0, 0.0, 0.0]])
+    input_tensor_b = tensor_type([[0.0, 2.0, 0.0], [0.0, 3.0, 0.0], [0.0, 4.0, 0.0]])
+
+    output_tensor = inverse_cdist(input_tensor_a, input_tensor_b)
+    assert type(output_tensor) == type(input_tensor_a)
+
+    output_tensor = to_numpy(output_tensor)
+
+    expected_tensor = numpy.array(
+        [[1.0 / numpy.sqrt(x * x + y * y) for y in [2.0, 3.0, 4.0]] for x in [3.0, 4.0]]
+    )
+
+    assert output_tensor.shape == expected_tensor.shape
+    assert numpy.allclose(output_tensor, expected_tensor)
+
+
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_pairwise_differences(tensor_type):
+
+    points_a = tensor_type([[0.0, 3.0, 1.0], [0.0, 4.0, 2.0]])
+    points_b = tensor_type([[2.0, 0.0, 2.0], [3.0, 0.0, 4.0], [4.0, 0.0, 0.0]])
+
+    output_tensor = pairwise_differences(points_a, points_b)
+
+    expected_tensor = numpy.array(
+        [
+            [[2.0, 2.0], [-3.0, -4.0], [1.0, 0.0]],
+            [[3.0, 3.0], [-3.0, -4.0], [3.0, 2.0]],
+            [[4.0, 4.0], [-3.0, -4.0], [-1.0, -2.0]],
+        ]
+    )
+
+    assert output_tensor.shape == expected_tensor.shape
+    assert numpy.allclose(output_tensor, expected_tensor)
+
+
+@pytest.mark.parametrize("tensor_type", tensor_types)
+def test_append_zero(tensor_type):
+
+    input_tensor = tensor_type([1.0, 2.0])
+    output_tensor = append_zero(input_tensor)
+
+    expected_tensor = numpy.array([1.0, 2.0, 0.0])
+
+    assert output_tensor.shape == expected_tensor.shape
+    assert numpy.allclose(output_tensor, expected_tensor)

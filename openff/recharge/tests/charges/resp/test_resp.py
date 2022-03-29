3c03cfc6e6fe470d7c31441123f6d7546a2d4a8f
SimonBoothroyd <simon.boothroyd@hotmail.com>
2022-03-29 12:52:50 +0100

    Fix generating dummy RESP initial charges

--- openff/recharge/tests/charges/resp/test_resp.py
+++ openff/recharge/tests/charges/resp/test_resp.py
@@ -68,15 +68,32 @@ def mock_esp_records() -> List[MoleculeESPRecord]:
     [
         ("[Cl:1][H:2]", [0.0, 0.0]),
         ("[O-:1][H:2]", [-0.5, -0.5]),
-        ("[N+:1]([H:2])([H:2])([H:2])([H:2])", [0.2, 0.05]),
+        ("[N+:1]([H:2])([H:2])([H:2])([H:2])", [0.2, 0.2]),
         ("[N+:1]([H:2])([H:3])([H:4])([H:5])", [0.2, 0.2, 0.2, 0.2, 0.2]),
         ("[N+:1]([H:2])([H:2])[O-:3]", [0.0, 0.0, 0.0]),
+        (
+            "[H:1][c:9]1[c:10]([c:13]([c:16]2[c:15]([c:11]1[H:3])[c:12]([c:14]"
+            "([c:17]([n+:20]2[C:19]([H:8])([H:8])[H:8])[C:18]([H:7])([H:7])[H:7])"
+            "[H:6])[H:4])[H:5])[H:2]",
+            [1.0 / 24.0] * 20,
+        ),
     ],
 )
 def test_generate_dummy_values(smiles, expected_values):
 
+    from simtk import unit as simtk_unit
+
     actual_values = _generate_dummy_values(smiles)
     assert actual_values == expected_values
+
+    molecule = Molecule.from_smiles(smiles, allow_undefined_stereo=True)
+
+    total_charge = molecule.total_charge.value_in_unit(simtk_unit.elementary_charge)
+    sum_charge = sum(
+        actual_values[i - 1] for i in molecule.properties["atom_map"].values()
+    )
+
+    assert numpy.isclose(total_charge, sum_charge)
 
 
 @pytest.mark.parametrize(

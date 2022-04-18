203f5bddcb24621f27f715cb0b3de5305b4ea891
SimonBoothroyd <simon.boothroyd@hotmail.com>
2022-04-18 10:49:31 +0100

    Add support for XTB charges (#114)

--- openff/recharge/charges/qc.py
+++ openff/recharge/charges/qc.py
@@ -1,14 +1,19 @@
 """Generate partial for molecules from a QC calculation."""
 import copy
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, cast
 
 import numpy
 from openff.units import unit
 from pydantic import BaseModel, Field
 from typing_extensions import Literal
 
+from openff.recharge.charges.exceptions import ChargeAssignmentError
+from openff.recharge.utilities.toolkits import get_atom_symmetries
+
 if TYPE_CHECKING:
     from openff.toolkit.topology import Molecule
+
+QCChargeTheory = Literal["am1", "am1bcc", "GFN1-xTB", "GFN2-xTB"]
 
 
 class QCChargeSettings(BaseModel):
@@ -16,7 +21,7 @@ class QCChargeSettings(BaseModel):
     quantum chemical calculations.
     """
 
-    theory: Literal["am1", "am1bcc"] = Field(
+    theory: QCChargeTheory = Field(
         "am1", description="The level of theory to use when computing the charges."
     )
 
@@ -35,6 +40,120 @@ class QCChargeSettings(BaseModel):
 class QCChargeGenerator:
     """A class which will compute the partial charges of a molecule
     from a quantum chemical calculation."""
+
+    @classmethod
+    def _symmetrize_charges(
+        cls, molecule: "Molecule", charges: numpy.ndarray
+    ) -> numpy.ndarray:
+        """Sets the charge on each atom to be the average value computed across all
+        charges on atoms with the same topological symmetry group.
+        """
+
+        symmetry_groups = get_atom_symmetries(molecule)
+
+        charges_by_group = {group: [] for group in symmetry_groups}
+
+        for group, charge in zip(symmetry_groups, charges):
+            charges_by_group[group].append(charge)
+
+        average_charges = {
+            group: float(numpy.mean(charges_by_group[group]))
+            for group in charges_by_group
+        }
+
+        return numpy.array([[average_charges[group]] for group in symmetry_groups])
+
+    @classmethod
+    def _check_connectivity(cls, molecule: "Molecule", conformer: unit.Quantity):
+
+        from qcelemental.molutil import guess_connectivity
+
+        expected_connectivity = {
+            tuple(sorted([bond.atom1_index, bond.atom2_index]))
+            for bond in molecule.bonds
+        }
+
+        symbols = numpy.array([atom.element.symbol.strip() for atom in molecule.atoms])
+
+        actual_connectivity = {
+            tuple(sorted(connection))
+            for connection in guess_connectivity(
+                symbols, conformer.m_as(unit.bohr), threshold=1.2
+            )
+        }
+
+        if actual_connectivity == expected_connectivity:
+            return
+
+        raise ChargeAssignmentError(
+            "The connectivity of the molecule changed when optimizing the initial "
+            "conformer and so the output charges will be incorrect."
+        )
+
+    @classmethod
+    def _generate_xtb_charges(
+        cls,
+        molecule: "Molecule",
+        conformer: unit.Quantity,
+        settings: QCChargeSettings,
+    ):
+
+        from qcelemental.models.common_models import DriverEnum, Model
+        from qcelemental.models.procedures import (
+            OptimizationInput,
+            OptimizationProtocols,
+            OptimizationResult,
+            QCInputSpecification,
+            TrajectoryProtocolEnum,
+        )
+        from qcelemental.models.results import AtomicInput, AtomicResult
+        from qcengine import compute, compute_procedure
+        from simtk import unit as openmm_unit
+
+        molecule = copy.deepcopy(molecule)
+        molecule._conformers = [conformer.m_as(unit.angstrom) * openmm_unit.angstrom]
+
+        qc_molecule = molecule.to_qcschema()
+
+        if settings.optimize:
+
+            optimization_schema = OptimizationInput(
+                initial_molecule=qc_molecule,
+                input_specification=QCInputSpecification(
+                    model=Model(method=settings.theory),
+                ),
+                protocols=OptimizationProtocols(
+                    trajectory=TrajectoryProtocolEnum.final
+                ),
+                keywords={"program": "xtb"},
+            )
+            optimization_results: OptimizationResult = cast(
+                OptimizationResult,
+                compute_procedure(optimization_schema, "geometric", raise_error=True),
+            )
+
+            cls._check_connectivity(
+                molecule, optimization_results.final_molecule.geometry * unit.bohr
+            )
+
+            results = optimization_results.trajectory[-1]
+        else:
+            input_schema = AtomicInput(
+                molecule=qc_molecule,
+                driver=DriverEnum.energy,
+                model=Model(method=settings.theory),
+            )
+
+            results = cast(AtomicResult, compute(input_schema, "xtb", raise_error=True))
+
+        charges = numpy.array(results.extras["xtb"]["mulliken_charges"]).reshape(
+            (-1, 1)
+        )
+
+        if settings.symmetrize:
+            charges = cls._symmetrize_charges(molecule, charges)
+
+        return charges
 
     @classmethod
     def _generate_omega_charges(
@@ -77,6 +196,39 @@ class QCChargeGenerator:
         )
 
     @classmethod
+    def _generate_am1_charges(
+        cls,
+        molecule: "Molecule",
+        conformer: numpy.ndarray,
+        settings: QCChargeSettings,
+    ):
+
+        from simtk import unit as simtk_unit
+
+        if settings.theory == "am1" and settings.optimize and settings.symmetrize:
+            charge_method = "am1-mulliken"
+        elif settings.theory == "am1bcc" and settings.optimize and settings.symmetrize:
+            charge_method = "am1-mulliken"
+        elif (
+            settings.theory == "am1bcc"
+            and not settings.optimize
+            and not settings.symmetrize
+        ):
+            charge_method = "am1bccnosymspt"
+        else:
+            charge_method = None
+
+        if charge_method:
+            molecule.assign_partial_charges(
+                charge_method, use_conformers=[conformer * simtk_unit.angstrom]
+            )
+            return numpy.array(
+                [*molecule.partial_charges.value_in_unit(simtk_unit.elementary_charge)]
+            )
+
+        return cls._generate_omega_charges(molecule, conformer, settings)
+
+    @classmethod
     def generate(
         cls,
         molecule: "Molecule",
@@ -105,8 +257,6 @@ class QCChargeGenerator:
             The computed partial charges.
         """
 
-        from simtk import unit as simtk_unit
-
         # Make a copy of the molecule so as not to perturb the original.
         molecule = copy.deepcopy(molecule)
 
@@ -114,42 +264,20 @@ class QCChargeGenerator:
 
         for conformer in conformers:
 
-            conformer = conformer[: molecule.n_atoms].m_as(unit.angstrom)
+            conformer = conformer[: molecule.n_atoms]
 
-            if settings.theory == "am1" and settings.optimize and settings.symmetrize:
-                charge_method = "am1-mulliken"
-            elif (
-                settings.theory == "am1bcc"
-                and settings.optimize
-                and settings.symmetrize
-            ):
-                charge_method = "am1-mulliken"
-            elif (
-                settings.theory == "am1bcc"
-                and not settings.optimize
-                and not settings.symmetrize
-            ):
-                charge_method = "am1bccnosymspt"
-            else:
-                charge_method = None
-
-            if charge_method:
-                molecule.assign_partial_charges(
-                    charge_method, use_conformers=[conformer * simtk_unit.angstrom]
-                )
+            if settings.theory in {"am1", "am1bcc"}:
                 conformer_charges.append(
-                    numpy.array(
-                        [
-                            *molecule.partial_charges.value_in_unit(
-                                simtk_unit.elementary_charge
-                            )
-                        ]
+                    cls._generate_am1_charges(
+                        molecule, conformer.m_as(unit.angstrom), settings
                     )
                 )
-            else:
+            elif settings.theory.lower().endswith("xtb"):
                 conformer_charges.append(
-                    cls._generate_omega_charges(molecule, conformer, settings)
+                    cls._generate_xtb_charges(molecule, conformer, settings)
                 )
+            else:
+                raise NotImplementedError()
 
         charges = numpy.mean(conformer_charges, axis=0).reshape(-1, 1)
         n_vsites = len(conformers[0]) - molecule.n_atoms
--- openff/recharge/tests/charges/test_qc.py
+++ openff/recharge/tests/charges/test_qc.py
@@ -4,9 +4,13 @@ import numpy
 import pytest
 from openff.toolkit.topology import Molecule
 from openff.units import unit
-from typing_extensions import Literal
 
-from openff.recharge.charges.qc import QCChargeGenerator, QCChargeSettings
+from openff.recharge.charges.exceptions import ChargeAssignmentError
+from openff.recharge.charges.qc import (
+    QCChargeGenerator,
+    QCChargeSettings,
+    QCChargeTheory,
+)
 
 
 @pytest.fixture(scope="module")
@@ -24,19 +28,70 @@ def methane() -> Tuple[Molecule, numpy.ndarray]:
     return molecule, conformer
 
 
+def test_check_connectivity(methane):
+
+    molecule, conformer = methane
+    conformer = conformer * unit.angstrom
+
+    QCChargeGenerator._check_connectivity(molecule, conformer)
+
+    with pytest.raises(
+        ChargeAssignmentError, match="The connectivity of the molecule changed"
+    ):
+        QCChargeGenerator._check_connectivity(molecule, conformer * 10.0)
+
+
+def test_symmetrize_charges(methane):
+
+    molecule, _ = methane
+
+    actual_charges = QCChargeGenerator._symmetrize_charges(
+        molecule, numpy.array([[-10.0], [1.0], [2.0], [3.0], [4.0]])
+    )
+    assert actual_charges.shape == (5, 1)
+
+    assert numpy.allclose(
+        actual_charges, numpy.array([[-10.0], [2.5], [2.5], [2.5], [2.5]])
+    )
+
+
+@pytest.mark.parametrize("theory", ["GFN1-xTB"])
+def test_generate_xtb_charges(methane, theory: QCChargeTheory):
+
+    molecule, conformer = methane
+    conformer = conformer * unit.angstrom
+
+    default_charges = QCChargeGenerator._generate_xtb_charges(
+        molecule, conformer, QCChargeSettings(theory=theory)
+    )
+    assert default_charges.shape == (5, 1)
+    assert not numpy.allclose(default_charges, 0.0)
+
+    asym_charges = QCChargeGenerator._generate_xtb_charges(
+        molecule, conformer, QCChargeSettings(theory=theory, symmetrize=False)
+    )
+    assert len({*default_charges[1:].flatten()}) == 1
+    assert len({*asym_charges[1:].flatten()}) == 4
+
+    unopt_charges = QCChargeGenerator._generate_xtb_charges(
+        molecule, conformer, QCChargeSettings(theory=theory, optimize=False)
+    )
+    assert not numpy.allclose(default_charges, unopt_charges)
+
+
 @pytest.mark.parametrize("theory", ["am1", "am1bcc"])
 def test_generate_omega_charges(methane, theory):
 
     molecule, conformer = methane
 
     default_charges = QCChargeGenerator._generate_omega_charges(
-        molecule, conformer, QCChargeSettings()
+        molecule, conformer, QCChargeSettings(theory=theory)
     )
     assert default_charges.shape == (5, 1)
     assert not numpy.allclose(default_charges, 0.0)
 
     asym_charges = QCChargeGenerator._generate_omega_charges(
-        molecule, conformer, QCChargeSettings(symmetrize=False)
+        molecule, conformer, QCChargeSettings(theory=theory, symmetrize=False)
     )
     assert len({*default_charges[1:].flatten()}) == 1
     assert len({*asym_charges[1:].flatten()}) == 4
@@ -47,19 +102,12 @@ def test_generate_omega_charges(methane, theory):
     assert not numpy.allclose(default_charges, unopt_charges)
 
 
-@pytest.mark.parametrize("theory", ["am1", "am1bcc"])
-def test_generate_charges(theory: Literal["am1", "am1bcc"], methane, monkeypatch):
+@pytest.mark.parametrize("theory", ["am1", "am1bcc", "GFN1-xTB"])
+def test_generate_charges(theory: QCChargeTheory, methane):
     """Ensure that charges can be generated for a simple molecule using
     the `QCChargeGenerator` class."""
 
     molecule, conformer = methane
-
-    def mock_generate_omega_charges(*args, **kwargs):
-        raise NotImplementedError()
-
-    monkeypatch.setattr(
-        QCChargeGenerator, "_generate_omega_charges", mock_generate_omega_charges
-    )
 
     charges = QCChargeGenerator.generate(
         molecule, [conformer * unit.angstrom], QCChargeSettings(theory=theory)

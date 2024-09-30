18e0bf47b8ccab9ac14b40fcd2bf2df4456e23f3
Matt Thompson <mattwthompson@protonmail.com>
2024-09-30 10:31:20 -0500

    Use new Pydantic v1 backdoor (#174)
    * Use new Pydantic v1 backdoor
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update version constraint
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- openff/recharge/_pydantic.py
+++ openff/recharge/_pydantic.py
@@ -1,18 +1,8 @@
-try:
-    from pydantic.v1 import (
-        BaseModel,
-        Field,
-        constr,
-        validator,
-        PositiveFloat,
-        ValidationError,
-    )
-except ModuleNotFoundError:
-    from pydantic import (  # type: ignore[assignment]
-        BaseModel,
-        Field,
-        constr,
-        validator,
-        PositiveFloat,
-        ValidationError,
-    )
+from pydantic.v1 import (
+    BaseModel,
+    Field,
+    constr,
+    validator,
+    PositiveFloat,
+    ValidationError,
+)
